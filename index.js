abaeb74a48db17534433afcf1f157163f158181d
Felix Ostrowski <felix.ostrowski@googlemail.com>
2017-01-03 19:39:25 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = function (kibana) {
+  return new kibana.Plugin({
+
+    name: 'oerworldmap-kibana',
+    require: ['kibana', 'elasticsearch'],
+    uiExports: {
+      // fieldFormat is not working as of Kibana 4.3.0, but it's fixed in the master branch
+      // see the issue on GitHub: https://github.com/elastic/kibana/issues/6040
+      // As a workaround, just add the field formats to visTypes array.
+      visTypes: ['plugins/oerworldmap-kibana/uuid'],
+      fieldFormats: ['plugins/oerworldmap-kibana/uuid']
+    }
+  });
+};
